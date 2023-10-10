a4ac8667937ae2958f5d2771d3781268f239ecb0
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-10 09:40:23 -0500

    Se realizo string interpolation. Esto nos permite utilizar variables, funciones y datos que podemos manipular entre courly breakers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fundamentals';
+  age = 18;
+  img =
+    'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80';
 }
