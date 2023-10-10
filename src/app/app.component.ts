a2b560a320b501534740d3c843d625d8d016be43
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-10 09:49:45 -0500

    Property binding, para establecer nuestras propiedades de html a tráves de []

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fundamentals';
+  name = 'Brandon';
   age = 18;
   img =
     'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80';
+  btnDisabled = true;
+  person = {
+    firstName: 'Brandon',
+    lastName: 'Agüero',
+    age: 19,
+    country: 'Peru',
+  };
 }
