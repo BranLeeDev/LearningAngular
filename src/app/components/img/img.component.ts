5b65d62beb44d15a2a73bbd630eb27119988d41d
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-12 14:40:14 -0500

    Componente para producto, usando input, creando modelos y eliminando los assets, y reemplando nuestras rutas locales pertenecientes a imagenes por imagenes en linea

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -9,7 +9,8 @@ export class ImgComponent {
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
 
-  imageDefault = '/assets/images/toy.jpg';
+  imageDefault =
+    'https://static.platzi.com/media/user_upload/toy-a0d1c980-a8ce-4fa4-83d6-3b016999a162.jpg';
 
   imgError() {
     this.img = this.imageDefault;
